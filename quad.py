b2295021580ece2da6cbc757f19ef3ef9ed3fd66
kedwards71 <61069606+kedwards71@users.noreply.github.com>
2020-11-08 21:25:26 -0600

    f

--- quad.py
+++ quad.py
@@ -1,41 +1,40 @@
 class quadrilateral:
     #Code
-    def _init_(self,name,side):
+    def _init_(self,name):
         self.name=name
-        self.side=side
 
 class kite(quadrilateral):
     #Code
-    def _init_(self,name,side):
-        super()._init_(name,side)
+    def _init_(self,name):
+        super()._init_(name)
 
 class trapezoid(quadrilateral):
     #Code
-    def _init_(self,name,side):
-        super()._init_(name,side)
+    def _init_(self,name):
+        super()._init_(name)
 
 class parallelogram(trapezoid):
     #Code
-    def _init_(self,name,side):
-        super()._init_(name,side)
+    def _init_(self,name):
+        super()._init_(name)
 
 class isosceles(trapezoid):
     #Code
-    def _init_(self,name,side):
-        super()._init_(name,side)
+    def _init_(self,name):
+        super()._init_(name)
 
 class rectangle(parallelogram,isosceles):
     #Code
-    def _init_(self,name,side):
-        super()._init_(name,side)
+    def _init_(self,name):
+        super()._init_(name)
 
 class rhombus(kite,parallelogram):
     #Code
-    def _init_(self,name,side):
-        super()._init_(name,side)
+    def _init_(self,name):
+        super()._init_(name)
 
 class square(rhombus,rectangle):
     #Code
-    def _init_(self,name,side):
-        super()._init_(name,side)
+    def _init_(self,name):
+        super()._init_(name)
         
